084e8e9a63f714fa6333ed3a608d8075ffbc75b6
LePande <119473273+LePande@users.noreply.github.com>
2023-09-01 22:25:03 -0500

    Tarea JS Version 1.2

--- js/bobada.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", (event) => {
-  event.preventDefault();
-
-  if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
-    console.log("«Ctrl + s» or «Command + s» combo");
-  }
-});
